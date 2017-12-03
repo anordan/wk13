b4670d06592059ffaa3d9c293af50bd126566d6b
ashley nordan <abuxbaum@mcad.edu>
2017-12-03 17:25:02 -0600

    wk13
    new map

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+$(document).ready(function(){
+	
+	//( referenced code from http://jsfiddle.net/n53qg/ )
+    //$('#capitols-nav a').click (function(evt) {
+     // $(".images").fadeIn (300);
+     //$("#" + $(this).data("rel")).fadeOut (1000);
+   // });
+	
+//});
+
+	
+$("#button1").click(function() {
+	$(".states1").before("<h4>Montgomery</h4>");
+		
+	});
+
+		
+$(".states1").click(function() {
+	$(".states1").slideUp("slow");
+		
+	});
+	
+
+$("#button2").click(function() {
+	$(".states2").before("<h4>Juneau</h4>");
+		
+	});
+$(".states2").click(function() {
+	$(".states2").slideUp("slow");
+		
+	});
+
+$("#button3").click(function() {
+	$(".states3").before("<h4>Phoenix</h4>");
+		
+	});
+$(".states3").click(function() {
+	$(".states3").slideUp("slow");
+		
+	});
+
+$("#button4").click(function() {
+	$(".states4").before("<h4>Little Rock</h4>");
+		
+	});
+$(".states4").click(function() {
+	$(".states4").slideUp("slow");
+		
+	});
+
+$("#button5").click(function() {
+	$(".states5").before("<h4>Los Angeles</h4>");
+		
+	});
+$(".states5").click(function() {
+	$(".states5").slideUp("slow");
+		
+	});
+
+$("#button6").click(function() {
+	$(".states6").before("<h4>Denver</h4>");
+		
+	});
+$(".states6").click(function() {
+	$(".states6").slideUp("slow");
+		
+	});
+	
+});
+
+
+
+
+
+
+
